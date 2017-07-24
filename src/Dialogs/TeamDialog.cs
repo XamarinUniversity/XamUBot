66500ebd43a46a2b5508fd4acb99d494ce0dabfb
Kym Phillpotts <kphillpotts@gmail.com>
2017-07-24 12:55:30 +1000

    Added Cards to team searches, including carousel for multiple results and a Lionel Ritchie for not found ;-)

--- src/Dialogs/TeamDialog.cs
+++ src/Dialogs/TeamDialog.cs
@@ -24,7 +24,7 @@ namespace XamUBot.Dialogs
 		{
 			var activity = await result;
 
-			var name = activity.Text;
+			var name = activity.Text.ToLowerInvariant();
 
 			if(name.Contains("exit"))
 			{
@@ -32,18 +32,63 @@ namespace XamUBot.Dialogs
 				return;
 			}
 
-			var teamMember = _teamList.FirstOrDefault(m => m.Name.ToLowerInvariant().Contains(name) || m.Email.ToLowerInvariant().Contains(name));
+            var teamMembers =_teamList.Where(m => m.Name.ToLowerInvariant().Contains(name) || m.Email.ToLowerInvariant().Contains(name));
 
-			if(teamMember != null)
+            if (teamMembers.Count() >= 1)
 			{
-				await context.PostAsync($"I found {teamMember.Name}. This is what {teamMember.Name} is doing: {teamMember.Description}");
-			}
+
+                var message = context.MakeMessage();
+
+                foreach (var person in teamMembers)
+                {
+                    message.Attachments.Add(CreateHeroCard(person).ToAttachment());
+                }
+
+                if (teamMembers.Count() > 1)
+                    message.AttachmentLayout = "carousel";
+
+                await context.PostAsync(message);
+            }
 			else
 			{
-				await context.PostAsync("Sorry, I don't know this. Are you looking for a team member? Just tell me the name.");
-			}
+				await context.PostAsync("Sorry, I couldn't find who you were looking for. Are you looking for a team member? Just tell me the name.");
+
+                var msg = context.MakeMessage();
+                msg.Attachments.Add(new Attachment()
+                {
+                    ContentUrl = @"http://i.imgur.com/QsIsjo8.jpg",
+                    ContentType = "image/jpg",
+                    Name = "Hello, is it me you're looking for"
+                });
+                await context.PostAsync(msg);
+
+            }
 
 			context.Wait(MessageReceivedAsync);
 		}
-	}
+
+        private HeroCard CreateHeroCard(TeamResponse teamMember)
+        {
+            var heroCard = new HeroCard
+            {
+                Title = teamMember.Name,
+                Subtitle = teamMember.Title,
+                Text = teamMember.ShortDescription,
+                Images = new List<CardImage> { new CardImage(teamMember.HeadshotUrl) },
+            };
+
+            // create buttons as appropriate
+            var buttons = new List<CardAction>();
+
+            if (!string.IsNullOrEmpty(teamMember.TwitterHandle))
+                buttons.Add(new CardAction(ActionTypes.OpenUrl, "Twitter Profile", value: teamMember.TwitterUrl));
+
+            if (!string.IsNullOrEmpty(teamMember.Website))
+                buttons.Add(new CardAction(ActionTypes.OpenUrl, "Website", value: teamMember.Website));
+
+            heroCard.Buttons = buttons;
+
+            return heroCard;
+        }
+    }
 }
