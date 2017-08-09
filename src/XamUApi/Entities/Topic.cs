a2ad7a7dd0e4edb9555ef400eb09c4e0fe73ff52
René Ruppert <rene.ruppert@gmail.com>
2017-08-09 16:31:16 +0200

    Start adding support to get class schedule

---
+++ src/XamUApi/Entities/Topic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace XamUApi
+{
+	/// <summary>
+	/// The C# representation of a topic.
+	/// The courses we offer are called topics. There are topics for iOS, Android etc.
+	/// Topics are scheduled regularly and you’re able to register.
+	/// Topics have a name, a description, a duration and more. A scheduled topic is called a “class” (see below).
+	/// </summary>
+	public class Topic
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string ShortDescription { get; set; }
+		public string Slug { get; set; }
+		public bool IsFeaturedClass { get; set; }
+		public string Track { get; set; }
+		public string VideoEmbedCode { get; set; }
+		public int DurationInMinutes { get; set; }
+		public List<ClassTime> ClassTimes { get; set; }
+	}
+
+	
+}
